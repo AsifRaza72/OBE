bf7c29ab6a038250348fad5319a88fac3898b4d9
AsifRaza72 <asifaligohari572@gmail.com>
2024-06-22 01:40:06 +0500

    modify login page

--- obe/src/components/LoginPage/Login.js
+++ obe/src/components/LoginPage/Login.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './Login.css'; // Import your CSS file for styling
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className='login-container'>
+      <h2>Login</h2>
+      <form>
+        <div className='form-group'>
+          <label htmlFor='email'>Email:</label>
+          <input type='email' id='email' name='email' required />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password:</label>
+          <input type='password' id='password' name='password' required />
+        </div>
+        <div className='form-group'>
+          <a href='/forgot-password'>Forgot Password?</a>
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
