b8e4c10aafca2efcd8ea65616229354f6e295453
AsifRaza72 <asifaligohari572@gmail.com>
2024-06-22 17:47:26 +0500

    login page modified

--- obe/src/components/LoginPage/Login.js
+++ obe/src/components/LoginPage/Login.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import './Login.css'; // Import your CSS file for styling
+import React,{useState} from "react";
+import "./Login.css"; // Import your CSS file for styling
 
-function Login() {
+const Login = () => {
+  const [email,SetEmail]=useState("")
+  const emailHandler=(e)=>{
+    SetEmail(e.target.value)
+    console.log(email)
+  }
+  console.log(email)
   return (
-    <div className='login-container'>
-      <h2>Login</h2>
-      <form>
-        <div className='form-group'>
-          <label htmlFor='email'>Email:</label>
-          <input type='email' id='email' name='email' required />
+    <div className="login-container">
+      <form className="form">
+      <div>
+        <h2>Login</h2>
         </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password:</label>
-          <input type='password' id='password' name='password' required />
+        <div className="form-group">
+          <label 
+          htmlFor="email"
+          onChange={emailHandler}
+          value={email}
+          >Email:</label>
+          <input type="email" id="email" name="email" required />
         </div>
-        <div className='form-group'>
-          <a href='/forgot-password'>Forgot Password?</a>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
         </div>
-        <button type='submit'>Login</button>
+        <div className="a form-group" >
+          <a href="/forgot-password" className="a">Forgot Password?</a>
+        </div>
+        <div className="button">
+          <button type="submit">Login</button>
+        </div>
       </form>
     </div>
   );
-}
+};
 
 export default Login;
